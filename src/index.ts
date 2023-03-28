1c18aca13ed7fb9bf5093b6e3c999f941ebc4d31
Alun Price <alunprice089@gmail.com>
2023-03-28 18:21:24 +0100

    added the bricks helper class; started to build out the level for view and bricks

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,3 @@
-import {
-  PADDLE_SPEED,
-  PADDLE_HEIGHT,
-  PADDLE_WIDTH,
-  PADDLE_STARTX,
-  BALL_SPEED,
-  BALL_SIZE,
-  BALL_STARTX,
-  BALL_STARTY,
-} from "./setup";
-import { CanvasView } from "./view/CanvasView";
-import { Ball } from "./sprites/Ball";
-import { Brick } from "./sprites/Brick";
-import { Paddle } from "./sprites/Paddle";
-// images
-import PADDLE_IMAGE from "./images/paddle.png";
-import BALL_IMAGE from "./images/ball.png";
-
 // level and colours
 import {
   PADDLE_SPEED,
@@ -27,6 +9,17 @@ import {
   BALL_STARTX,
   BALL_STARTY,
 } from "./setup";
+import { CanvasView } from "./view/CanvasView";
+import { Ball } from "./sprites/Ball";
+import { Brick } from "./sprites/Brick";
+import { Paddle } from "./sprites/Paddle";
+
+// images
+import PADDLE_IMAGE from "./images/paddle.png";
+import BALL_IMAGE from "./images/ball.png";
+
+// Helpers
+import { createBricks } from "./helpers";
 
 let gameOver = false;
 let score = 0;
@@ -43,12 +36,27 @@ function setGameWin(view: CanvasView) {
 
 function gameLoop(
   view: CanvasView,
-  bricks: Brick[],
-  paddle: Paddle,
-  ball: Ball
-) {}
+  bricks: Brick[]
+  //paddle: Paddle,
+  //ball: Ball
+) {
+  view.clear();
+  view.drawBricks(bricks);
 
-function startGame(view: CanvasView) {}
+  requestAnimationFrame(() => gameLoop(view, bricks));
+}
+
+function startGame(view: CanvasView) {
+  // reset the displays when start the game
+  score = 0;
+  view.drawInfo(""); // first in the variable
+  view.drawScore(0); // show the user
+
+  // create all the bricks
+  const bricks = createBricks();
+
+  gameLoop(view, bricks);
+}
 
 // Create a new View
 const view = new CanvasView("#playField");
