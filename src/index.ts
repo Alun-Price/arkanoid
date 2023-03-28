ced8ab1da19bafd1c114adbe8e2023312284ba56
Alun Price <alunprice089@gmail.com>
2023-03-28 17:25:18 +0100

    scaffolded index.ts and built the brick class

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,55 @@
-// Start here
-console.log("Start here!");
+import {
+  PADDLE_SPEED,
+  PADDLE_HEIGHT,
+  PADDLE_WIDTH,
+  PADDLE_STARTX,
+  BALL_SPEED,
+  BALL_SIZE,
+  BALL_STARTX,
+  BALL_STARTY,
+} from "./setup";
+import { CanvasView } from "./view/CanvasView";
+import { Ball } from "./sprites/Ball";
+import { Brick } from "./sprites/Brick";
+import { Paddle } from "./sprites/Paddle";
+// images
+import PADDLE_IMAGE from "./images/paddle.png";
+import BALL_IMAGE from "./images/ball.png";
+
+// level and colours
+import {
+  PADDLE_SPEED,
+  PADDLE_WIDTH,
+  PADDLE_HEIGHT,
+  PADDLE_STARTX,
+  BALL_SPEED,
+  BALL_SIZE,
+  BALL_STARTX,
+  BALL_STARTY,
+} from "./setup";
+
+let gameOver = false;
+let score = 0;
+
+function setGameOver(view: CanvasView) {
+  view.drawInfo("GAME OVER!!");
+  gameOver = false;
+}
+
+function setGameWin(view: CanvasView) {
+  view.drawInfo("YOU WIN!!");
+  gameOver = false;
+}
+
+function gameLoop(
+  view: CanvasView,
+  bricks: Brick[],
+  paddle: Paddle,
+  ball: Ball
+) {}
+
+function startGame(view: CanvasView) {}
+
+// Create a new View
+const view = new CanvasView("#playField");
+view.initStartButton(startGame);
